5e1e492cc3f5b85cd7268d39d3b705368cd3ff4e
ismail bozkurt <ismailbozk@gmail.com>
2014-10-19 13:29:29 +0300

    in basekinectmodel class, test code pieces were removed.

--- Models/BaseKinectModel.cpp
+++ Models/BaseKinectModel.cpp
@@ -18,24 +18,22 @@ BaseKinectModel BaseKinectModel::BaseKinectModelWithGrayImage(cv::Mat grayImage,
 
 	for (int i = 0; i < rawDepthData.size() ; i++)
 	{
-		if (i == 2259)
-		{
-			bool c = false;
-		}
+		if (rawDepthData[i] > 1)
+		{//if raw depth data is valid. the very first generetion kinect currently can detect distance between 1000 mm to 3500 mm 
+			int x = i % model.grayImage.cols;
+			int y = i / model.grayImage.cols;
+			cv::Point3d point3D = DimensionConverter::DepthTo3D(x, y, rawDepthData[i]);
 
-		int x = i % model.grayImage.cols;
-		int y = i / model.grayImage.cols;
-		cv::Point3d point3D = DimensionConverter::DepthTo3D(x, y, rawDepthData[i]);
+			cv::Point2i calibratedXYPoint = DimensionConverter::From3DPointToRGBCameraFrame(point3D);		//coordinates on gray image
 
-		cv::Point2i calibratedXYPoint = DimensionConverter::From3DPointToRGBCameraFrame(point3D);		//coordinates on gray image
+			if (calibratedXYPoint.x >= 0 && calibratedXYPoint.x < model.grayImage.cols &&
+				calibratedXYPoint.y >= 0 && calibratedXYPoint.y < model.grayImage.rows)
+			{//if calibratedImage point is intersects with the original image dimensions
+				int calibratedIndex = (calibratedXYPoint.y * model.grayImage.cols) + calibratedXYPoint.x;
 
-		if (calibratedXYPoint.x >= 0 && calibratedXYPoint.x < model.grayImage.cols &&
-			calibratedXYPoint.y >= 0 && calibratedXYPoint.y < model.grayImage.rows)
-		{//if calibratedImage point is intersects with the original image dimensions
-			int calibratedIndex = (calibratedXYPoint.y * model.grayImage.cols) + calibratedXYPoint.x;
-
-			model.calibratedDepth[calibratedIndex] = rawDepthData[i];
-			model.pointCloud[calibratedIndex] = point3D;
+				model.calibratedDepth[calibratedIndex] = rawDepthData[i];
+				model.pointCloud[calibratedIndex] = point3D;
+			}
 		}
 	}
 
